42df88d042b22de8a25ad363e36f89c639f4b4b5
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-09-10 08:29:34 -0500

    Fixing the typo

--- pyscf/df/grad/sacasscf.py
+++ pyscf/df/grad/sacasscf.py
@@ -312,7 +312,7 @@ def _grad_elec_df_response_direct(mc, mf_grad, dms, pair_weights,
     '''
     mol = mc.mol
     auxmol = mc.with_df.auxmol
-    nao, nbas = mol.nao, mol.nbas
+    nao, nbas, naux = mol.nao, mol.nbas, auxmol.nao
     dms = np.asarray(dms).reshape(-1, nao, nao)
     nset = len(dms)
     pair_weights = np.asarray(pair_weights)
